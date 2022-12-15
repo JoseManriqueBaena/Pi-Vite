f9b57fa090e802b8a026e90d0a9898da762c64bf
JoseManriqueBaena <josed3h@gmail.com>
2022-12-15 15:02:49 -0500

    prueba deploy 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,12 +7,12 @@ import { Provider } from 'react-redux';
 import store from './redux/store/index';
 // import dotenv from 'dotenv';
 // dotenv.config();
-// const { VITE_LOCAL } = process.env;
+// const { VITE_LOCAL_URL } = process.env;
 
 axios.defaults.baseURL =
-	process.env.VITE_DEPLOY || import.meta.env.VITE_LOCAL_URL;
-// axios.defaults.baseURL = import.meta.env.VITE_LOCAL_URL;
+	import.meta.env.VITE_DEPLOY || import.meta.env.VITE_LOCAL_URL;
 // console.log(import.meta.env.VITE_LOCAL_URL);
+console.log(import.meta.env.VITE_DEPLOY);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
